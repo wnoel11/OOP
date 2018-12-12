146e13cf6e41eca29964105680bb124c2e47d78d
wnoel11 <43223925+wnoel11@users.noreply.github.com>
2018-12-11 19:38:31 -0500

    Add files via upload

---
+++ OOP/Car.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Car
+    {
+        private int Speed;
+        private string Make;
+        private string Model;
+        private int Year;
+
+        public Car(string Make, string Model, int Year, int Speed)
+        {
+            this.Make = Make;
+            this.Model = Model;
+            this.Year = Year;
+            this.Speed = Speed;
+        }
+
+        public Car(string Make, string Model, int Year): this (Make, Model, Year, 0){}
+        
+        public int SpeedUp()
+        {
+            
+            {
+                return Speed++;
+            }
+        
+        }
+                
+        public int SlowDown()
+        {
+            if (Speed > 0)
+            {
+                return Speed--;
+            }
+            return 0; 
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(Year + " " + Make + " " + Model + " is going " + Speed + " MPH.");
+        }
+
+
+
+    }
+}
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -10,6 +10,29 @@ namespace OOP
     {
         static void Main(string[] args)
         {
+            int car1Speed = 20;
+            int car2Speed = 0;
+            Car car1 = new Car("Ford", "Focus", 2010, car1Speed);
+            Car car2 = new Car("Chevy", "Cruze", 2018, car2Speed);
+            for (int i = 0; i < 60; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    car2Speed= car2.SpeedUp();
+                }
+                if (i % 3 == 0)
+                {
+                    car1Speed = car1.SpeedUp();
+                }
+                if (i % 5 == 0)
+                {
+                    car1Speed = car1.SlowDown();
+                    car2Speed = car2.SlowDown();
+                }
+            }
+            car1.Display();
+            car2.Display();
+
         }
     }
 }
